35a82eb2acaa07d168c4c216ebe37087c4025c25
Moritz Lotze <lotzemoritz@gmail.com>
2017-03-17 20:31:28 +0100

    remove attribution

--- km3astro/constants.py
+++ km3astro/constants.py
@@ -1,14 +1,5 @@
 """Constants, like geographical positions."""
 
-__author__ = "Moritz Lotze"
-__copyright__ = "Copyright 2017, Moritz Lotze and the KM3NeT collaboration."
-__credits__ = []
-__license__ = "BSD-3"
-__maintainer__ = "Moritz Lotze"
-__email__ = "mlotze@km3net.de"
-__status__ = "Development"
-
-
 orca_longitude = 42 + (48/60)  # degree
 orca_latitude = 6 + (2/60)  # degree
 orca_height = -2450  # m
