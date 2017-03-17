099fb89199635f20cb144b10b51f640b7b28a1ea
Moritz Lotze <lotzemoritz@gmail.com>
2017-03-17 12:00:47 +0100

    clean up coord trafos

--- km3astro/coord.py
+++ km3astro/coord.py
@@ -1,50 +1,53 @@
+"""Coordinate transformations.
+"""
 from astropy.time import Time
-from astropy.units import rad, deg  # noqa
+from astropy.units import rad, deg, hourangle  # noqa
 from astropy.coordinates import (EarthLocation, SkyCoord, AltAz, Longitude,
-                                 Latitude)
+                                 Latitude, get_sun, get_moon)
 import numpy as np
 
-from km3astro.constants import orca_longitude, orca_latitude, orca_height
-from km3astro.time import np_to_datetime, random_date
+from km3astro.constants import (
+    arca_longitude, arca_latitude, arca_height,
+    orca_longitude, orca_latitude, orca_height,
+)
+from km3astro.time import np_to_datetime, random_date, np_to_astrotime
+
+
+ARCA_LOC = EarthLocation.from_geodetic(
+    Longitude(arca_longitude * deg),
+    Latitude(arca_latitude * deg),
+    height=arca_height
+)
+
+ORCA_LOC = EarthLocation.from_geodetic(
+    Longitude(orca_longitude * deg),
+    Latitude(orca_latitude * deg),
+    height=orca_height
+)
+
+
+def transform_to_orca(event, time):
+    time = np_to_astrotime(time)
+    orca_frame = AltAz(obstime=time, location=ORCA_LOC)
+    return event.transform_to(orca_frame)
 
 
 def orca_event(azimuth, time, zenith):
     """Create astropy events from detector coordinates."""
-    time = np.atleast_1d(time)
     zenith = np.atleast_1d(zenith)
     azimuth = np.atleast_1d(azimuth)
 
-    orca_loc = EarthLocation.from_geodetic(
-        Longitude(orca_longitude * deg),
-        Latitude(orca_latitude * deg),
-        height=orca_height
-    )
-    time = Time(np_to_datetime(time))
-    orca_frame = AltAz(obstime=time, location=orca_loc)
+    time = np_to_astrotime(time)
+    orca_frame = AltAz(obstime=time, location=ORCA_LOC)
 
     altitude = zenith - np.pi/2
     event = SkyCoord(alt=altitude*rad, az=azimuth*rad, frame=orca_frame)
     return event
 
 
-def gc_dist(event):
-    """Returns distance to galactic center, in ICRS coordinates.
-
-    If no events (SkyCoord
-    """
-    events = event.icrs
-    gc = SkyCoord(0*deg, 0*deg, frame='galactic').icrs
-    return events.separation(gc).radian
-
-
-def orca_gc_dist(azimuth, time, zenith):
-    evt = orca_event(azimuth, time, zenith)
-    dist = gc_dist(evt)
-    return dist
-
-
 def random_azimuth(n=1, unit='rad'):
-    azi = (np.pi * np.random.random_sample(size=n))
+    """Draw azimuth, uniformly distributed."""
+    azi = (2 * np.pi * np.random.random_sample(size=n))
     if unit == 'rad':
         return azi
     elif unit == 'deg':
@@ -53,11 +56,29 @@ def random_azimuth(n=1, unit='rad'):
         raise KeyError("Unknown unit '{}'".format(unit))
 
 
-def gc_dist_random(zenith, time='random', azimuth='random'):
-    n_evts = len(zenith)
-    if time == 'random':
-        time = random_date(n=n_evts)
-    if azimuth == 'random':
-        azimuth = random_azimuth(n=n_evts)
-    dist = orca_gc_dist(azimuth, time, zenith)
-    return dist
+def random_zenith(n=1, unit='rad'):
+    """Draw zenith, uniformly distributed in cos(zen)."""
+    coszen = (2 * np.random.random_sample(size=n) - 1)
+    zen = np.arccos(coszen)
+    if unit == 'rad':
+        return zen
+    elif unit == 'deg':
+        return zen / np.pi * 180
+    else:
+        raise KeyError("Unknown unit '{}'".format(unit))
+
+
+def sun_in_orca(time):
+    time = np_to_astrotime(time)
+    orca_frame = AltAz(obstime=time, location=ORCA_LOC)
+    sun = get_sun(time)
+    sun_orca = sun.transform_to(orca_frame)
+    return sun_orca
+
+
+def gc_in_orca(time):
+    time = np_to_astrotime(time)
+    orca_frame = AltAz(obstime=time, location=ORCA_LOC)
+    gc = SkyCoord(0*deg, 0*deg, frame='galactic')
+    gc_orca = gc.transform_to(orca_frame)
+    return gc_orca
