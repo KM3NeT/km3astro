4d67758d74876b64c789463fc1c15bb6e6eec942
Moritz Lotze <lotzemoritz@gmail.com>
2017-03-17 12:00:21 +0100

    add docstring

--- km3astro/__init__.py
+++ km3astro/__init__.py
@@ -1,1 +1,7 @@
+"""
+Tools for astronomy with km3net -- coordinate transformations etc.
+
+This library build on top of astropy, which in turn is built
+upon IAUSOFA et al.
+"""
 __version__ = '0.2.1'
