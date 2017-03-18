c9345f12be118773607639f735adb87ff70dd704
Moritz Lotze <lotzemoritz@gmail.com>
2017-03-18 19:17:13 +0100

    Bump version number

--- km3astro/__init__.py
+++ km3astro/__init__.py
@@ -1,7 +1,1 @@
-"""
-Tools for astronomy with km3net -- coordinate transformations etc.
-
-This library build on top of astropy, which in turn is built
-upon IAUSOFA et al.
-"""
-__version__ = '0.2.0'
+__version__ = '0.3.0'
