daf191430af3dac6f29318fbfb4e3c028dd45630
Moritz Lotze <lotzemoritz@gmail.com>
2017-07-26 15:43:31 +0200

    add utm coordinates

--- km3astro/constants.py
+++ km3astro/constants.py
@@ -1,13 +1,30 @@
 """Constants, like geographical positions."""
 
-datum = 'WGS84'
+import utm
+
+HEMISPHERE = 'north'
+DATUM = 'WGS84'
 
 orca_latitude = 42 + (48 / 60)  # degree
 orca_longitude = 6 + (2 / 60)  # degree
 orca_height = -2450  # m
-orca_utm_zone = '32T'
+
+# no longer needed (computet from latlon)
+#orca_utm_zone_number = 32
+#orca_utm_zone_letter = 'T'
+orca_utm_x, orca_utm_y, orca_utm_zone_number, orca_utm_zone_letter = \
+    utm.from_latlon(orca_latitude, orca_longitude)
+orca_utm_zone = '{num}{let}'.format(
+    num=orca_utm_zone_number, let=orca_utm_zone_letter)
 
 arca_latitude = 36 + (16 / 60)  # degree
 arca_longitude = 10 + (6 / 60)  # degree
 arca_height = -3500  # m
-arca_utm_zone = '33S'
+
+# no longer needed (computet from latlon)
+#arca_utm_zone_number = 33
+#arca_utm_zone_letter = 'S'
+arca_utm_x, arca_utm_y, arca_utm_zone_number, arca_utm_zone_letter = \
+    utm.from_latlon(arca_latitude, arca_longitude)
+arca_utm_zone = '{num}{let}'.farmat(
+    num=arca_utm_zone_number, let=arca_utm_zone_letter)
