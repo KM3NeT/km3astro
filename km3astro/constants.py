963d73ef0909fee3613d030dd1f647803a04a9cf
Moritz Lotze <lotzemoritz@gmail.com>
2017-07-26 14:55:36 +0200

    add utm zones

--- km3astro/constants.py
+++ km3astro/constants.py
@@ -1,9 +1,13 @@
 """Constants, like geographical positions."""
 
-orca_latitude = 42 + (48/60)  # degree
-orca_longitude = 6 + (2/60)  # degree
-orca_height = -2450  # m
+datum = 'WGS84'
 
-arca_latitude = 36 + (16/60)  # degree
-arca_longitude = 10 + (6/60)  # degree
+orca_latitude = 42 + (48 / 60)  # degree
+orca_longitude = 6 + (2 / 60)  # degree
+orca_height = -2450  # m
+orca_utm_zone = '32T'
+
+arca_latitude = 36 + (16 / 60)  # degree
+arca_longitude = 10 + (6 / 60)  # degree
 arca_height = -3500  # m
+arca_utm_zone = '33S'
