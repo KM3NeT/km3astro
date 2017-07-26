bc2e5f3da8e8b677c1b6208b620f8e9fa2c29430
Moritz Lotze <lotzemoritz@gmail.com>
2017-07-26 18:30:48 +0200

    add orca UTM coordinates

--- km3astro/tests/test_constants.py
+++ km3astro/tests/test_constants.py
@@ -13,25 +13,18 @@ arca_longitude_naive = 16 + (6 / 60)  # degree
 arca_lat_delta = 0.1
 arca_lon_delta = 0.2
 
-# taken from antares (should be the same)
-orca_utm_zone_number_ref = 32
-orca_utm_zone_letter_ref = 'T'
-
-antares_easting = 268221.6
-antares_northing = 4742381.9
-
-orca_antares_delta_east = 12000    # m
-orca_antares_delta_north = 600    # m
+orca_latitude_naive = 42 + (48.05 / 60)  # degree
+orca_longitude_naive = 6 + (1.3368 / 60)  # degree
+orca_lat_delta = 0.01
+orca_lon_delta = 0.01
 
 
 class TestUTM(TestCase):
     def test_ref_vs_computed(self):
-        self.assertEqual(orca_utm_zone_number, orca_utm_zone_number_ref)
-        self.assertEqual(orca_utm_zone_letter, orca_utm_zone_letter_ref)
-        self.assertAlmostEqual(orca_easting, antares_easting,
-                               delta=orca_antares_delta_east)      # 10 km
-        self.assertAlmostEqual(orca_northing, antares_northing,
-                               delta=orca_antares_delta_north)
+        self.assertAlmostEqual(orca_longitude_naive, orca_longitude,
+                               delta=orca_lon_delta)
+        self.assertAlmostEqual(orca_latitude_naive, orca_latitude,
+                               delta=orca_lat_delta)
         self.assertAlmostEqual(arca_longitude_naive, arca_longitude,
                                delta=arca_lon_delta)
         self.assertAlmostEqual(arca_latitude_naive, arca_latitude,
