f4c786e8318763f66e763fbe50c80c630d9ee90b
Moritz Lotze <lotzemoritz@gmail.com>
2017-08-01 15:15:44 +0200

    bump doc config

--- doc/conf.py
+++ doc/conf.py
@@ -40,6 +40,8 @@ extensions = [
     'sphinx_gallery.gen_gallery',
 ]
 
+autosummary_generate = True
+
 
 exclude_patterns = ['_build', '**.ipynb_checkpoints']
 
@@ -150,7 +152,7 @@ html_title = 'km3astro'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #
-html_short_title = None
+# html_short_title = None
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
@@ -250,67 +252,6 @@ html_sidebars = {
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'km3astrodoc'
 
-# -- Options for LaTeX output ---------------------------------------------
-
-latex_elements = {
-     # The paper size ('letterpaper' or 'a4paper').
-     #
-     # 'papersize': 'letterpaper',
-
-     # The font size ('10pt', '11pt' or '12pt').
-     #
-     # 'pointsize': '10pt',
-
-     # Additional stuff for the LaTeX preamble.
-     #
-     # 'preamble': '',
-
-     # Latex figure (float) alignment
-     #
-     # 'figure_align': 'htbp',
-}
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title,
-#  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, 'km3astro.tex', 'km3astro Documentation',
-     'Moritz Lotze', 'manual'),
-]
-
-# The name of an image file (relative to this directory) to place at the top of
-# the title page.
-#
-# latex_logo = None
-
-# For "manual" documents, if this is true, then toplevel headings are parts,
-# not chapters.
-#
-# latex_use_parts = False
-
-# If true, show page references after internal links.
-#
-# latex_show_pagerefs = False
-
-# If true, show URL addresses after external links.
-#
-# latex_show_urls = False
-
-# Documents to append as an appendix to all manuals.
-#
-# latex_appendices = []
-
-# It false, will not define \strong, \code, 	itleref, \crossref ... but only
-# \sphinxstrong, ..., \sphinxtitleref, ... To help avoid clash with user added
-# packages.
-#
-# latex_keep_old_macro_names = True
-
-# If false, no module index is generated.
-#
-# latex_domain_indices = True
-
-
 # -- Options for manual page output ---------------------------------------
 
 # One entry per manual page. List of tuples
@@ -355,12 +296,12 @@ texinfo_documents = [
 sphinx_gallery_conf = {
     # path to store the module using example template
     'backreferences_dir': 'modules/generated',
-    'doc_module': ('km3astro', ),
+    'doc_module': ('km3astro', 'numpy', 'astropy'),
     'examples_dirs': ['../examples'],
     'gallery_dirs': ['auto_examples'],
-#     'reference_url':  {
-#         'km3astro': None,
-#         'matplotlib': 'http://matplotlib.org',
-#         'numpy': 'http://docs.scipy.org/doc/numpy-1.9.1',
-#     }
+    'reference_url': {
+        'km3astro': None,
+        'numpy': 'http://docs.scipy.org/doc/numpy',
+        'astropy': 'http://docs.astropy.org',
+    }
 }
