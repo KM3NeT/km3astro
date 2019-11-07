d2b74dda635ef09b0b4e8b3cd6a828b92ec6ecb5
Tamas Gal <tgal@km3net.de>
2019-11-07 13:22:15 +0100

    Fix python deps

--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setup(
     license='BSD-3',
     packages=['km3astro', ],
     install_requires=requirements,
-    python_requires='>=3.6',
+    python_requires='>=3.5',
     include_package_data=True,
 )
