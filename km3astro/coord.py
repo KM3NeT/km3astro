Fix true azimuth calculation

km3astro/coord.py
@@ -171,7 +171,7 @@ def local_event(azimuth, time, zenith, radian=True, location="orca", **kwargs):
 
     loc = get_location(location)
     # neutrino telescopes call the co-azimuth "azimuth"
-    true_azimuth = (np.pi / 2 - azimuth + convergence_angle(loc.lat.rad, loc.lon.rad)) % (np.pi)
+    true_azimuth = (np.pi / 2 - azimuth + np.pi + convergence_angle(loc.lat.rad, loc.lon.rad)) % (2*np.pi)
     frame = local_frame(time, location=location)
     event = SkyCoord(alt=altitude * rad, az=true_azimuth * rad, frame=frame, **kwargs)
     return event