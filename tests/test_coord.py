b8b1642ad731b9b552f88e3ea945842d9f02ad86
Hichem Tedjditi <htedjditi@km3net.de>
2022-04-29 17:13:17 +0200

    Change tests/test_coord.py: Previous error = data_path could not find coordinate_systems_benchmark

--- tests/test_coord.py
+++ tests/test_coord.py
@@ -66,31 +66,41 @@ class TestAntaresBenchmark(TestCase):
         self.angle_threshold = 0.02
         
     def test_antares_objects(self):
-        ktt.test_angle_separation(data_path("astro/antares_astro_objects_benchmark.csv"),"antares","antares")
+        ktt.test_angle_separation(
+            data_path("astro/antares_astro_objects_benchmark.csv"), "antares", "antares"
+        )
 
     def test_antares_coordinate_system_benchmarks(self):
-        ktt.test_angle_separation(data_path("astro/antares_coordinate_system_benchmarks.csv"),"antares","antares")
-
-
+        ktt.test_angle_separation(
+            data_path("astro/antares_coordinate_systems_benchmark.csv"),"antares","antares"
+        )
 class TestARCABenchmark(TestCase):
     def setUp(self):
         self.angle_threshold = 0.02
         
     def test_arca_objects(self):
-        ktt.test_angle_separation(data_path("astro/ARCA_astro_objects_benchmark.csv"),"arca","arca")
+        ktt.test_angle_separation(
+            data_path("astro/ARCA_astro_objects_benchmark.csv"), "arca", "arca"
+        )
 
     def test_arca_coordinate_system_benchmarks(self):
-        ktt.test_angle_separation(data_path("astro/ARCA_coordinate_system_benchmarks.csv"),"arca","arca")
+        ktt.test_angle_separation(
+            data_path("astro/ARCA_coordinate_systems_benchmark.csv"), "arca", "arca"
+        )
 
 class TestORCABenchmark(TestCase):
     def setUp(self):
         self.angle_threshold = 0.02
         
     def test_orca_objects(self):
-        ktt.test_angle_separation(data_path("astro/ORCA_astro_objects_benchmark.csv"),"orca","orca")
+        ktt.test_angle_separation(
+            data_path("astro/ORCA_astro_objects_benchmark.csv"), "orca", "orca"
+        )
 
     def test_orca_coordinate_system_benchmarks(self):
-        ktt.test_angle_separation(data_path("astro/ORCA_coordinate_system_benchmarks.csv"),"orca","orca")
+        ktt.test_angle_separation(
+            data_path("astro/ORCA_coordinate_systems_benchmark.csv"), "orca", "orca"
+        )
 
 
 class TestAntaresBenchmark_previous(TestCase):
